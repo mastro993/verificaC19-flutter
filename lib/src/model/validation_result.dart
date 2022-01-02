c8975ede8456a81996197c3da9a757646eb742f5
Federico Mastrini <federicomastrini93@gmail.com>
2022-01-02 18:33:05 +0100

    Removed unnecessary import

--- lib/src/model/validation_result.dart
+++ lib/src/model/validation_result.dart
@@ -1,5 +1,4 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:verificac19/src/model/certificate_status.dart';
 import 'package:verificac19/verificac19.dart';
 
 part 'validation_result.freezed.dart';
