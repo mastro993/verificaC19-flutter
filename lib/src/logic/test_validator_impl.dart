7538f1b1e4ed83e88e4d0489e0ff98bc6f6df0d6
Patrick Nicolosi <patricknicolosi99@gmail.com>
2022-02-18 18:50:35 +0100

    Fixed validation logics problems for green certificate from test

--- lib/src/logic/test_validator_impl.dart
+++ lib/src/logic/test_validator_impl.dart
@@ -82,7 +82,7 @@ class TestValidatorImpl implements TestValidator {
 
     final validityStart =
         test.dateTimeOfCollection.add(Duration(hours: startHours));
-    final validityEnd = test.dateTimeOfCollection.add(Duration(days: endHours));
+    final validityEnd = test.dateTimeOfCollection.add(Duration(hours: endHours));
 
     if (validityStart > clock.now()) {
       log('Test result is not valid yet, starts at ${validityStart.toIso8601String()}');
