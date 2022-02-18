b27e89bcc9ce30a467c89526d1e952b64957255b
Patrick Nicolosi <patricknicolosi99@gmail.com>
2022-02-18 18:00:24 +0100

    Update test_validator_impl.dart

--- lib/src/logic/test_validator_impl.dart
+++ lib/src/logic/test_validator_impl.dart
@@ -25,10 +25,10 @@ class TestValidatorImpl implements TestValidator {
     required DateTime dateOfBirth,
   }) async {
     try {
-      if (mode == ValidationMode.normalDGP ||
+      if (mode == ValidationMode.superDGP ||
           mode == ValidationMode.boosterDGP ||
           mode == ValidationMode.schoolDGP) {
-        log('Not valid. Super DGP required');
+        log('Not valid. Super or Booster DGP required');
         return GreenCertificateStatus.notValid;
       }
 
