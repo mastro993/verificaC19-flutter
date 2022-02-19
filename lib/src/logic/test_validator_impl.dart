e6f46c4f3c56296b7c2b47d6cb5e80ce75308583
Federico Mastrini <federicomastrini93@gmail.com>
2022-02-19 11:44:28 +0100

    Added RSA visitors validation mode

--- lib/src/logic/test_validator_impl.dart
+++ lib/src/logic/test_validator_impl.dart
@@ -26,7 +26,8 @@ class TestValidatorImpl implements TestValidator {
   }) async {
     try {
       if (mode == ValidationMode.superDGP ||
-          mode == ValidationMode.boosterDGP) {
+          mode == ValidationMode.boosterDGP ||
+          mode == ValidationMode.visitorsRSADGP) {
         log('Not valid. Super or Booster DGP required');
         return GreenCertificateStatus.notValid;
       }
